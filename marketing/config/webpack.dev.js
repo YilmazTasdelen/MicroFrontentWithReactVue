8b25b20fea68f26ab2183c0e0b858a1bf409f230
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-12-17 18:14:13 +0300

    .

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,38 +1,30 @@
 const { merge } = require('webpack-merge');
-/**Merge is a function that we can use to merge together
-two different webpack config objects.
-So this merge function is what's going to
-allow us to take all the config that we just wrote out
-inside that common file and merge it together
-with a configuration that we're about to write
-inside this development file. */
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-
+const packageJson = require('../package.json');
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8081,
-        historyApiFallback: {
-            index: 'index.html',
-        },
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html',
     },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'marketing',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './MarketingApp': './src/bootstrap',
-            },
-            shared: ['react', 'react-dom'],
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
